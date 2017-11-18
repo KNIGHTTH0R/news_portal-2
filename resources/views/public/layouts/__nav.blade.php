bf35041076662f9b02d97ef0f808d574c0edd391
unknown <korneenko.andrew@gmail.com>
2017-11-19 01:08:53 +0200

    beta

--- resources/views/public/layouts/__nav.blade.php
+++ resources/views/public/layouts/__nav.blade.php
@@ -14,61 +14,63 @@
                     <a class="navbar-brand btn btn-primary backToSite" href="{{ url('/') }}">На главную</a>
             @endif
         </div>
-        <div class="mar-auto btn-group ">
-            <div class="btn-group">
-                <a href="{{action('IndexController@analyticalNews')}}" class="btn btn-danger">Аналитические статьи</a>
-                <button type="button" class="btn btn-danger dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <span class="sr-only">Toggle Dropdown</span>
-                </button>
-                <div class="dropdown-menu">
-                    @foreach($analytical as $item)
-                        <li class="dropdown-item dropdown-submenu">
-                            <a tabindex="-1" class="dropdown-item" href="{{ url('analytical/'.$item->slug) }}">{{ $item->title }}</a>
-                            <ul class="dropdown-menu">
-                                <h6 class="dropdown-header">{{ $item->category->name }}</h6>
-                                @foreach($item->category->news()->limit(5)->get() as $var)
-                                    <li class="dropdown-item dropdown-submenu">
-                                        <a href="{{ action('IndexController@show', ['category' => $item->category->slug, 'slug' => $var->slug]) }}"
-                                            bypass class="dropdown-item interaction" data-module="git" data-action="status">
-                                            <i class="fa fa-fw fa-flag"></i>&nbsp; {{ $var->title }}
-                                        </a>
-                                        <ul class="dropdown-menu">
-                                            <h6 class="dropdown-header">Тэги</h6>
-                                                @foreach($var->tag as $tag)
-                                                    <li>
-                                                        <a href="{{ action('IndexController@newsFromTag', ['tag' => $tag->name]) }}"
-                                                        bypass class="dropdown-item interaction" data-module="git" data-action="status">
-                                                            <i class="fa fa-fw fa-flag"></i>&nbsp; {{ $tag->name }}
-                                                        </a>
-                                                    </li>
-                                                @endforeach
-                                        </ul>
-                                    </li>
-                                @endforeach
-                            </ul>
-                        </li>
-                    @endforeach
+        <div class="mar-auto btn-group">
+            @if ($analytical->isNotEmpty())
+                <div class="btn-group">
+                    <a href="{{action('IndexController@analyticalNews')}}" class="btn btn-danger">Аналитические статьи</a>
+                    <button type="button" class="btn btn-danger dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <span class="sr-only">Toggle Dropdown</span>
+                    </button>
+                    <div class="dropdown-menu">
+                            @foreach($analytical as $item)
+                                <li class="dropdown-item dropdown-submenu">
+                                    <a tabindex="-1" class="dropdown-item" href="{{ url('analytical/'.$item->slug) }}">{{ $item->title }}</a>
+                                    <ul class="dropdown-menu">
+                                        <h6 class="dropdown-header">{{ $item->category->name }}</h6>
+                                        @foreach($item->category->news()->limit(5)->get() as $var)
+                                            <li class="dropdown-item dropdown-submenu">
+                                                <a href="{{ action('IndexController@show', ['category' => $item->category->slug, 'slug' => $var->slug]) }}"
+                                                    bypass class="dropdown-item interaction" data-module="git" data-action="status">
+                                                    <i class="fa fa-fw fa-flag"></i>&nbsp; {{ $var->title }}
+                                                </a>
+                                                <ul class="dropdown-menu">
+                                                    <h6 class="dropdown-header">Тэги</h6>
+                                                        @foreach($var->tag as $tag)
+                                                            <li>
+                                                                <a href="{{ action('IndexController@newsFromTag', ['tag' => $tag->name]) }}"
+                                                                bypass class="dropdown-item interaction" data-module="git" data-action="status">
+                                                                    <i class="fa fa-fw fa-flag"></i>&nbsp; {{ $tag->name }}
+                                                                </a>
+                                                            </li>
+                                                        @endforeach
+                                                </ul>
+                                            </li>
+                                        @endforeach
+                                    </ul>
+                                </li>
+                            @endforeach
+                    </div>
                 </div>
-            </div>
-                @if(isset($category))
-                    @foreach($category as $key => $item)
-                        @if ($item->isNotEmpty())
-                            <div class="btn-group">
-                                <a href="{{action('IndexController@newsFromCategory', ['slug' => $item->first()->category->slug])}}"
-                                   class="btn btn-danger">{{ $item->first()->category->name }}
-                                </a>
-                                <button type="button" class="btn btn-danger dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <span class="sr-only">Toggle Dropdown</span>
-                                </button>
-                                <div class="dropdown-menu">
-                                    @foreach($item as $new)
-                                        <a class="dropdown-item" href="{{ url($item->first()->category->slug .'/'.$new->slug) }}">{{ $new->title }}</a>
-                                    @endforeach
-                                </div>
+            @endif
+            @if(isset($category))
+                @foreach($category as $key => $item)
+                    @if ($item->isNotEmpty())
+                        <div class="btn-group">
+                            <a href="{{action('IndexController@newsFromCategory', ['slug' => $item->first()->category->slug])}}"
+                               class="btn btn-danger">{{ $item->first()->category->name }}
+                            </a>
+                            <button type="button" class="btn btn-danger dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <span class="sr-only">Toggle Dropdown</span>
+                            </button>
+                            <div class="dropdown-menu">
+                                @foreach($item as $new)
+                                    <a class="dropdown-item" href="{{ url($item->first()->category->slug .'/'.$new->slug) }}">{{ $new->title }}</a>
+                                @endforeach
                             </div>
-                        @endif
-                    @endforeach
-                @endif
+                        </div>
+                    @endif
+                @endforeach
+            @endif
         </div>
 
     </div>
